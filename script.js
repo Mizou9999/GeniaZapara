e9e8d9f844e538bff2bac9dc4600cac130a2971d
mizou <mizou9999@yandex.ru>
2019-10-02 15:57:31 +0300

    responsive header + js toggle navbar

---
+++ script.js
@@ -0,0 +1,24 @@
+const navbar = document.getElementById("nav_bar")
+window.onscroll = () => scrollNav()
+const scrollNav = () => {
+	let currentScroll = document.documentElement.scrollTop || window.pageYOffset
+
+	if (currentScroll > 250) {
+		navbar.classList.add("solid")
+	} else if (currentScroll < 250) {
+		navbar.classList.remove("solid")
+	}
+}
+
+const toggle = document.getElementById("toggle")
+const openNav = event => {
+	event.preventDefault()
+	const mobileNav = document.getElementById("mobile_nav_ul")
+	console.log("he")
+}
+
+toggle.addEventListener("click", function() {
+	const mobileNav = document.getElementById("mobile_nav_ul")
+	mobileNav.classList.toggle("toggle")
+	console.log("clicked")
+})
